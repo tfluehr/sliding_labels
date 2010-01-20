d613b5a804b053f6e2cc0a50e8277a87a67868ed
Tim Fluehr <tfluehr@gmail.com>
2010-01-20 14:05:41 -0500

    more hacks for vertical demo

--- src/sliding_label.js
+++ src/sliding_label.js
@@ -7,100 +7,107 @@
   
   // TODO: tests
   SlidingLabels = Class.create({
-    initialize: function(elementId, options) {
+    initialize: function(elementId, options){
       form = $(elementId);
-      
-      // default options list.
-      this.options = Object.extend({
-          labelInColor: '#999',
-          labelOutColor: '#000',
-          restingPosition: 5,
-          crossPosition: 6,
-          effectDuration: 0.2,
-          labelAdjustment: 10,
-          orientation: 'horizontal'
-      }, options);
-      
-      if (this.options.orientation == 'vertical'){
-          
+      var defaultOptions = {
+        labelInColor: '#999',
+        labelOutColor: '#000',
+        restingPosition: 5,
+        crossPosition: 6,
+        effectDuration: 0.2,
+        labelAdjustment: 10,
+        orientation: 'horizontal'
+      };
+      if (options.orientation == 'vertical') {
+        var temp = defaultOptions.restingPosition;
+        defaultOptions.restingPosition = defaultOptions.crossPosition;
+        defaultOptions.crossPosition = temp;
       }
-     form.select('.slider label').each(function(label){
-          // style the label with JS for progressive enhancement
-          if (this.options.orientation == 'vertical'){
-              label.setStyle({
-                  'color': this.options.labelInColor,
-                  'position': 'absolute',
-                  'left': this.options.crossPosition + 'px',
-                  'top': this.options.restingPosition + 'px',
-                  'display': 'inline',
-                  'zIndex': '99'
-              });
-          }
-          else{
-              label.setStyle({
-                  'color': this.options.labelInColor,
-                  'position': 'absolute',
-                  'top': this.options.crossPosition + 'px',
-                  'left': this.options.restingPosition + 'px',
-                  'display': 'inline',
-                  'zIndex': '99'
-              });
-          }
+      // default options list.
+      this.options = Object.extend(defaultOptions, options);
+      
+      form.select('.slider label').each((function(label){
+        // style the label with JS for progressive enhancement
+        if (this.options.orientation == 'vertical') {
+          label.setStyle({
+            'color': this.options.labelInColor,
+            'position': 'absolute',
+            'left': this.options.crossPosition + 'px',
+            'top': this.options.restingPosition + 'px',
+            'display': 'inline',
+            'zIndex': '99'
+          });
+        }
+        else {
+          label.setStyle({
+            'color': this.options.labelInColor,
+            'position': 'absolute',
+            'top': this.options.crossPosition + 'px',
+            'left': this.options.restingPosition + 'px',
+            'display': 'inline',
+            'zIndex': '99'
+          });
+        }
+        
+        // grab the input value
+        var input = label.next('input'), inputval = input.getValue();
+        
+        // grab the label width, then add 5 pixels to it
+        var labelwidth = label.getWidth(), labelmove = labelwidth + this.options.labelAdjustment;
+        
+        //onload, check if a field is filled out, if so, move the label out of the way
+        if (!inputval.blank()) {
+          label.morph('left:' + (this.options.restingPosition - labelmove) + 'px; color:' + this.options.labelOutColor + ';');
+        }
+        
+        // if the input is empty on focus move the label to the left
+        // if it's empty on blur, move it back
+        input.observe('focus', (function(ev){
+          var input = ev.element();
+          var label = input.previous('label');
+          var value = input.getValue();
           
-          // grab the input value
-          var input = label.next('input'), inputval = input.getValue();
-
-          // grab the label width, then add 5 pixels to it
-          var labelwidth = label.getWidth(), labelmove = labelwidth + this.options.labelAdjustment;
-
-          //onload, check if a field is filled out, if so, move the label out of the way
-          if (!inputval.blank()) {
-            label.morph('left:' + (this.options.restingPosition - labelmove) + 'px; color:' + this.options.labelOutColor + ';');
-          }
-          
-          // if the input is empty on focus move the label to the left
-          // if it's empty on blur, move it back
-          input.observe('focus', function(ev){
-              var input = ev.element();
-              var label = input.previous('label');
-              var value = input.getValue();
-                
-              if (this.options.orientation == 'vertical') {
-                  var width = label.getWidth();
-                  var adjust = width + this.options.labelAdjustment;
-                  if (value === '') {
-                      label.morph('left:' + (this.options.restingPosition - adjust) + 'px;color:' + this.options.labelOutColor + ';');
-                  }
-                  else {
-                      label.setStyle({
-                          'left': (-adjust) + 'px',
-                          'color': this.options.labelOutColor
-                      });
-                  }
-              }
-              else {
-                  var width = label.getWidth();
-                  var adjust = width + this.options.labelAdjustment;
-                  if (value === '') {
-                      label.morph('left:' + (this.options.restingPosition - adjust) + 'px;color:' + this.options.labelOutColor + ';');
-                  }
-                  else {
-                      label.setStyle({
-                          'left': (-adjust) + 'px',
-                          'color': this.options.labelOutColor
-                      });
-                  }
-              }
-          }.bindAsEventListener(this)).observe('blur', function(ev){
-            var input = ev.element();
-            var label = input.previous('label');
-            var value = input.getValue();
-            
+          if (this.options.orientation == 'vertical') {
+            var height = label.getHeight();
+            var hAdjust = height + this.options.labelAdjustment;
             if (value.blank()) {
+              input.morph('margin-top:' + (this.options.restingPosition + hAdjust) + 'px;color:' + this.options.labelOutColor + ';');
+            }
+            else {
+              label.setStyle({
+                'left': (-adjust) + 'px',
+                'color': this.options.labelOutColor
+              });
+            }
+          }
+          else {
+            var width = label.getWidth();
+            var adjust = width + this.options.labelAdjustment;
+            if (value.blank()) {
+              label.morph('left:' + (this.options.restingPosition - adjust) + 'px;color:' + this.options.labelOutColor + ';');
+            }
+            else {
+              label.setStyle({
+                'left': (-adjust) + 'px',
+                'color': this.options.labelOutColor
+              });
+            }
+          }
+        }).bindAsEventListener(this)).observe('blur', (function(ev){
+          var input = ev.element();
+          var label = input.previous('label');
+          var value = input.getValue();
+          
+          if (value.blank()) {
+            if (this.options.orientation == 'vertical') {
+              input.morph('margin-top:' + (0) + 'px; color:' + this.options.labelInColor + ';');
+            }
+            else{
               label.morph('left:' + (this.options.restingPosition) + 'px; color:' + this.options.labelInColor + ';');
             }
-          }.bindAsEventListener(this));
-      }.bind(this));
+          }
+        }).bindAsEventListener(this));
+      }).bind(this));
     }
   });
 })();
